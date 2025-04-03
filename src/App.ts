bdd3e0e1a5eb1b6020e07cfe7acedcb249d77279
m5production <dimikh@mail.ru>
2025-04-03 12:57:44 +0300

    feat: Add CORS

--- src/App.ts
+++ src/App.ts
@@ -2,9 +2,12 @@ import express from 'express'
 import gameRoutes from './routes/gameRoutes'
 import questionRoutes from './routes/questionRoutes'
 import userRoutes from './routes/userRoutes'
+import cors from 'cors'
 import { errorHandler } from './middlewares/errorHandler'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
